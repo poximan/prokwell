14412f6c9ca5fc152df06b8e2379d24d61877536
hugo donato <poxi_man@yahoo.com>
2019-10-06 00:12:28 -0300

    precesamiento de log

--- archivo.js
+++ archivo.js
@@ -1,40 +1,36 @@
 var fs_origen = require('fs')
 var fs_destino = require('fs')
 
-var data_destino;
-
-const PATH_ORIG = "D://documentos//hugo//trabajo//proyectos//se//et soc rural//mb//PCB-Log.txt"
-const PATH_DEST = "D://documentos//hugo//trabajo//proyectos//se//et soc rural//mb//PCB-Log-proc.txt"
-const EXPRESION = /((\[[0-9A-F]+\])(\\r\\n)?){8}/gm;
+const PATH_ORIG = "D://documentos//hugo//git//prokwell//test//PCB-Log.txt"
+const PATH_DEST = "D://documentos//hugo//git//prokwell//test//PCB-Log-proc.txt"
+const EXPRESION = /(\[[0-9A-F]+\]){8}/gm;
 
 fs_origen.readFile(PATH_ORIG, "utf-8", (err, data_origen) => {
-  var preguntas = data_origen.toString().match(EXPRESION)
 
-  var output
-  var indices = [0]
+  const entrada = data_origen.toString().replace(/\r\n/gm,'')
+  const preguntas = entrada.match(EXPRESION)
+
+  let salida = ""
+  let indices = [0] // arranca en indice 0 para no perder encabezado de PCB-Log
 
   preguntas.forEach((element, indice) => {
 
     let ind_inicio = 0
-    if(indices.length > 0) ind_inicio = indices[indice-1]
+    if(indices.length > 1) ind_inicio = indices[indice-1]
 
-    indices.push(data_origen.indexOf(element, ind_inicio))
+    indices.push(entrada.indexOf(element, ind_inicio))
   })
 
   indices.forEach((element, indice) => {
-    output = [data_origen.toString().slice(0, element), '\n', data_origen.slice(element)].join('');
+    const pregunta = entrada.slice(element, element + 32) + "\n"
+    salida += pregunta
+    salida += entrada.slice(element + 32, indices[indice + 1]) + "\n\n"
   })
 
-  console.log(preguntas);
-  console.log(indices);
-  console.log(output);
+  console.log(salida);
+  console.log(`se encontraron ${preguntas.length} preguntas en ${indices.length - 1} indices`);
 
-  fs_destino.writeFile(PATH_DEST, data_destino, (err) => {
+  fs_destino.writeFile(PATH_DEST, salida, (err) => {
     if (err) console.log(err);
-    //console.log(data_destino.toString());
   });
 });
-
-// busca patron trama pregunta modbus
-// /(\[[0-9A-F]+\]){8}/g
-// ((\[[0-9A-F]+\])(\\r)?(\\n)?){8}
