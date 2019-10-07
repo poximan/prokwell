8736a1b923186a17d3238f1a09c3dcff403ce984
hugo donato <poxi_man@yahoo.com>
2019-10-06 21:17:37 -0300

    agregando renombrado automatico de nuevo archivo

--- archivo.js
+++ archivo.js
@@ -2,12 +2,11 @@ var fs_origen = require('fs')
 var fs_destino = require('fs')
 
 const PATH_ORIG = "D://documentos//hugo//git//prokwell//test//PCB-Log.txt"
-const PATH_DEST = "D://documentos//hugo//git//prokwell//test//PCB-Log-proc.txt"
 const EXPRESION = /(\[[0-9A-F]+\]){8}/gm;
 
 fs_origen.readFile(PATH_ORIG, "utf-8", (err, data_origen) => {
 
-  const entrada = data_origen.toString().replace(/\r\n/gm,'')
+  const entrada = data_origen.toString().replace(/\r\n/gm, '')
   const preguntas = entrada.match(EXPRESION)
 
   let salida = ""
@@ -27,10 +26,12 @@ fs_origen.readFile(PATH_ORIG, "utf-8", (err, data_origen) => {
     salida += entrada.slice(element + 32, indices[indice + 1]) + "\n\n"
   })
 
-  console.log(salida);
   console.log(`se encontraron ${preguntas.length} preguntas en ${indices.length - 1} indices`);
 
-  fs_destino.writeFile(PATH_DEST, salida, (err) => {
+  let path_dest = PATH_ORIG
+  path_dest = path_dest.slice(0, PATH_ORIG.length - 4) + "-proc" + path_dest.slice(PATH_ORIG.length - 4);
+
+  fs_destino.writeFile(path_dest, salida, (err) => {
     if (err) console.log(err);
   });
 });
